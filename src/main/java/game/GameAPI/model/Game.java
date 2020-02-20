12770952a64d418dd81dc19fbe172519a94ab165
Mcurci95 <mcurci95@gmail.com>
2020-02-19 23:56:10 -0800

    id and name and json properties

--- src/main/java/game/GameAPI/model/Game.java
+++ src/main/java/game/GameAPI/model/Game.java
@@ -1,5 +1,26 @@
 package game.GameAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.UUID;
+
 public class Game {
 
+    private final UUID id;
+    private final String name;
+
+
+    public Game(@JsonProperty("id") UUID id,
+                       @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
