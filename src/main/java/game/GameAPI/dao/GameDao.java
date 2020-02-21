b647e77f5d6a63ee404ea3f0b40e9d042dd1190f
Mcurci95 <mcurci95@gmail.com>
2020-02-20 17:36:42 -0800

    Game interface

--- src/main/java/game/GameAPI/dao/GameDao.java
+++ src/main/java/game/GameAPI/dao/GameDao.java
@@ -1,4 +1,18 @@
 package game.GameAPI.dao;
 
-public class GameDao {
+import game.GameAPI.model.Game;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GameDao {
+
+    int insertGame(UUID id, Game game);
+
+    default int insetGame(Game game) {
+        UUID id = UUID.randomUUID();
+        return insertGame(id, game);
+    }
+
+    List<Game> selectAllGames();
 }
